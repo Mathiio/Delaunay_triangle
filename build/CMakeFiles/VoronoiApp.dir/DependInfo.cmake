3f0af40e03c402b6d871921d75d27cb0cc22b9f8
Mathiio <matheo.pougalan@gmail.com>
2023-04-21 11:37:09 +0200

    init: projet

---
+++ build/CMakeFiles/VoronoiApp.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/matheo/Documents/IMAC/S2/Prog-Algo/Delaunay_triangle/src/SDL2_gfxPrimitives.c" "CMakeFiles/VoronoiApp.dir/src/SDL2_gfxPrimitives.c.o" "gcc" "CMakeFiles/VoronoiApp.dir/src/SDL2_gfxPrimitives.c.o.d"
+  "/Users/matheo/Documents/IMAC/S2/Prog-Algo/Delaunay_triangle/src/application_ui.cpp" "CMakeFiles/VoronoiApp.dir/src/application_ui.cpp.o" "gcc" "CMakeFiles/VoronoiApp.dir/src/application_ui.cpp.o.d"
+  "/Users/matheo/Documents/IMAC/S2/Prog-Algo/Delaunay_triangle/src/main.cpp" "CMakeFiles/VoronoiApp.dir/src/main.cpp.o" "gcc" "CMakeFiles/VoronoiApp.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/VoronoiApp.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "/Users/matheo/Documents/IMAC/S2/Prog-Algo/Delaunay_triangle/bin/Debug/VoronoiApp"
+  "/Users/matheo/Documents/IMAC/S2/Prog-Algo/Delaunay_triangle/bin/Debug/VoronoiApp.pdb"
+  "CMakeFiles/VoronoiApp.dir/src/SDL2_gfxPrimitives.c.o"
+  "CMakeFiles/VoronoiApp.dir/src/SDL2_gfxPrimitives.c.o.d"
+  "CMakeFiles/VoronoiApp.dir/src/application_ui.cpp.o"
+  "CMakeFiles/VoronoiApp.dir/src/application_ui.cpp.o.d"
+  "CMakeFiles/VoronoiApp.dir/src/main.cpp.o"
+  "CMakeFiles/VoronoiApp.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/VoronoiApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
