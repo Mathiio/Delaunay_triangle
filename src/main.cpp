6068bed0ee496f105a808676c487a5e1531b11ea
Marianne KERCKHOVE <29842502+Maria30@users.noreply.github.com>
2023-06-04 21:19:05 +0200

    add : final code

--- src/main.cpp
+++ src/main.cpp
@@ -5,67 +5,69 @@
 #include <map>
 #include <queue>
 #include <algorithm>
+#include <ctime>
+using namespace std;
 
 #define EPSILON 0.0001f
 
-struct Coords
-{
+// Définissez VORONOI en fonction de ce que vous voulez voir
+// 0 : Triangles Delaunay
+// 1 : Diagramme Voronoi avec segments
+// 2 : Diagramme Voronoi avec polygones
+#define VORONOI 2
+
+struct Coords {
     int x, y;
 
-    bool operator==(const Coords& other)
-    {
+    bool operator==(const Coords& other) {
         return x == other.x and y == other.y;
     }
 };
 
-struct Segment
-{
+struct Segment {
     Coords p1, p2;
 };
 
-struct Triangle
-{
+struct Triangle {
     Coords p1, p2, p3;
     bool complet=false;
 };
 
-struct Application
-{
+struct Polygon {
+    Coords p1, p2, p3;
+    int r, g, b;
+};
+
+struct Application {
     int width, height;
     Coords focus{100, 100};
 
-    std::vector<Coords> points;
-    std::vector<Triangle> triangles;
+    vector<Coords> points;
+    vector<Coords> centers;
+    vector<Segment> edges;
+    vector<Triangle> triangles;
+    vector<Polygon> polygons;
 };
 
 
-void swapTriangles(std::vector<Triangle>& triangles, int i, int j) {
-    std::swap(triangles[i], triangles[j]);
+void swapTriangles(vector<Triangle>& triangles, int i, int j) {
+    swap(triangles[i], triangles[j]);
 }
 
-void swapSegments(Segment segments[], int i, int j) {
-    std::swap(segments[i], segments[j]);
-}
-
-bool compareCoords(Coords point1, Coords point2)
-{
+bool compareCoords(Coords point1, Coords point2) {
     if (point1.y == point2.y)
         return point1.x < point2.x;
     return point1.y < point2.y;
 }
 
-void drawPoints(SDL_Renderer *renderer, const std::vector<Coords> &points)
-{
-    for (std::size_t i = 0; i < points.size(); i++)
-    {
-        filledCircleRGBA(renderer, points[i].x, points[i].y, 3, 240, 240, 23, SDL_ALPHA_OPAQUE);
+void drawPoints(SDL_Renderer *renderer, const vector<Coords> &points, int r, int g, int b) {
+    for (size_t i = 0; i < points.size(); i++) {
+        filledCircleRGBA(renderer, points[i].x, points[i].y, 3, r, g, b, SDL_ALPHA_OPAQUE);
     }
 }
 
-void drawSegments(SDL_Renderer *renderer, const std::vector<Segment> &segments)
-{
-    for (std::size_t i = 0; i < segments.size(); i++)
-    {
+void drawSegments(SDL_Renderer *renderer, const vector<Segment> &segments) {
+    for (size_t i = 0; i < segments.size(); i++) {
         lineRGBA(
             renderer,
             segments[i].p1.x, segments[i].p1.y,
@@ -74,10 +76,18 @@ void drawSegments(SDL_Renderer *renderer, const std::vector<Segment> &segments)
     }
 }
 
-void drawTriangles(SDL_Renderer *renderer, const std::vector<Triangle> &triangles)
-{
-    for (std::size_t i = 0; i < triangles.size(); i++)
-    {
+void drawEdges(SDL_Renderer *renderer, const vector<Segment> &edges) {
+    for (size_t i = 0; i < edges.size(); i++)     {
+        lineRGBA(
+            renderer,
+            edges[i].p1.x, edges[i].p1.y,
+            edges[i].p2.x, edges[i].p2.y,
+            240, 240, 20, SDL_ALPHA_OPAQUE);
+    }
+}
+
+void drawTriangles(SDL_Renderer *renderer, const vector<Triangle> &triangles) {
+    for (size_t i = 0; i < triangles.size(); i++) {
         const Triangle& t = triangles[i];
         trigonRGBA(
             renderer,
@@ -89,14 +99,36 @@ void drawTriangles(SDL_Renderer *renderer, const std::vector<Triangle> &triangle
     }
 }
 
-void draw(SDL_Renderer *renderer, const Application &app)
-{
+void drawPolygons(SDL_Renderer *renderer, const vector<Polygon> &polygons) {
+    for (size_t i = 0; i < polygons.size(); i++) {
+        const Polygon& p = polygons[i];
+        filledTrigonRGBA(
+            renderer,
+            p.p1.x, p.p1.y,
+            p.p2.x, p.p2.y,
+            p.p3.x, p.p3.y,
+            p.r, p.g, p.b, SDL_ALPHA_OPAQUE
+        );
+    }
+}
+
+void draw(SDL_Renderer *renderer, const Application &app) {
     /* Remplissez cette fonction pour faire l'affichage du jeu */
     int width, height;
     SDL_GetRendererOutputSize(renderer, &width, &height);
 
-    drawPoints(renderer, app.points);
-    drawTriangles(renderer, app.triangles);
+    if (VORONOI == 0) {
+        drawTriangles(renderer, app.triangles);
+    }  
+    else if (VORONOI == 1) {
+        drawTriangles(renderer, app.triangles);
+        drawPoints(renderer, app.centers, 255, 0, 0);
+        drawEdges(renderer, app.edges);
+    }
+    else if (VORONOI == 2) {
+        drawPolygons(renderer, app.polygons);
+    }
+    drawPoints(renderer, app.points, 240, 240, 23);
 }
 
 /*
@@ -164,73 +196,213 @@ bool CircumCircle(
     return ((drsqr - *rsqr) <= EPSILON ? true : false);
 }
 
-void construitVoronoi(Application &app)
-{
-    float posxCentre, posyCentre, rayon;
-    int countSegments = 0;
-    std::sort(app.points.begin(), app.points.end(), compareCoords);
-    app.triangles.clear();
-    app.triangles.push_back({{-1000, -1000}, {500, 3000}, {1500, -1000}});
+void construitVoronoi(Application &app) {
+    srand((unsigned) time(NULL));
+    sort(app.points.begin(), app.points.end(), compareCoords);
 
-    for(int i=0; i<app.points.size(); i++){
-        Segment LS[100];
-        for(int j=0; j<app.triangles.size(); j++){
-            if(CircumCircle(app.points[i].x, app.points[i].y, 
-            app.triangles[j].p1.x, app.triangles[j].p1.y, 
-            app.triangles[j].p2.x, app.triangles[j].p2.y, 
-            app.triangles[j].p3.x, app.triangles[j].p3.y,
-            &posxCentre, &posyCentre, &rayon)){
-                LS[countSegments] = {app.triangles[j].p1, app.triangles[j].p2};
-                countSegments++;
-                LS[countSegments] = {app.triangles[j].p2, app.triangles[j].p3};
-                countSegments++;
-                LS[countSegments] = {app.triangles[j].p1, app.triangles[j].p3};
-                countSegments++;
-                swapTriangles(app.triangles, j, (app.triangles.size()-1));
-                app.triangles.pop_back();
+    // Vider la liste existante de triangles
+    app.triangles.clear();
+
+    // Créer un très grand triangle et le rajouter à la liste de triangles déjà créés
+    Triangle grandTriangle = {{-1000, -1000}, {500, 3000}, {1500, -1000}};
+    app.triangles.push_back(grandTriangle);
+
+    for(size_t i = 0; i < app.points.size(); i++){
+
+        // ******************** //
+        // *TRIANGLES DELAUNAY* //
+        // ******************** //
+
+        // Créer une liste de segments LS
+        vector<Segment> LS;
+
+        // Pour chaque triangle T déjà créé :
+        for(size_t j = 0; j < app.triangles.size(); j++){
+            // Tester si le cercle circonscrit contient le point P
+            float posxCentre, posyCentre, rayon;          
+            if (CircumCircle(app.points[i].x, app.points[i].y, 
+                            app.triangles[j].p1.x, app.triangles[j].p1.y, 
+                            app.triangles[j].p2.x, app.triangles[j].p2.y, 
+                            app.triangles[j].p3.x, app.triangles[j].p3.y,
+                            &posxCentre, &posyCentre, &rayon)) {
+                // Récupérer les différents segments de ce triangle dans LS
+                Segment s1 = {{app.triangles[j].p1.x, app.triangles[j].p1.y},{app.triangles[j].p2.x,app.triangles[j].p2.y}};
+                Segment s2 = {{app.triangles[j].p2.x, app.triangles[j].p2.y},{app.triangles[j].p3.x,app.triangles[j].p3.y}};
+                Segment s3 = {{app.triangles[j].p3.x, app.triangles[j].p3.y},{app.triangles[j].p1.x,app.triangles[j].p1.y}};
+                LS.push_back(s1);
+                LS.push_back(s2);
+                LS.push_back(s3);
+
+                // Enlever le triangle T de la liste 
+                app.triangles.erase(app.triangles.begin() + j);
+                j--;
             }
         }
-        for(int k=0; k<countSegments; k++) {
-            for (int l = 0; l < countSegments; l++){
-                if((LS[k].p1 == LS[l].p2) && (LS[k].p2 == LS[l].p1)) {
-                    swapSegments(LS, k, l);
-                    LS.pop();
-                    countSegments--;
-                }
-            }    
+
+        // Pour chaque segment S de la liste LS
+        for (size_t k = 0; k < LS.size(); k++) {
+            for (size_t j = 0; j < LS.size(); j++) {
+                // Si un segment partage ses points avec un autre segment, les virer
+                if(k == j) break;
+                    
+                if (LS[k].p1 == LS[j].p2 && LS[k].p2 == LS[j].p1) {
+                   LS.erase(LS.begin() + j);
+                   j--;
+                   k--;
+                   LS.erase(LS.begin() + k);
+                } 
+            }
         }
-        for(int l=0; l<countSegments; l++) {
-            app.triangles.push_back({LS[l].p1, LS[l].p2, app.points[i]});
+
+        // Pour chaque segment S de la liste LS
+        for(size_t l = 0; l < LS.size(); l++) {
+            // Créer un nouveau triangle composé du segment S et du point P
+            Triangle newTriangle = {LS[l].p1, LS[l].p2, app.points[i]};
+            app.triangles.push_back(newTriangle);
+        }  
+
+        // Vider la liste existante de centres
+        app.centers.clear();
+
+        // Pour chaque triangle T déjà créé :
+        for(size_t m = 0; m < app.triangles.size(); m++) {
+            // On récupère les coordonnées du centre de son cercle circonscrit
+            float posxCentre, posyCentre, rayon;
+            CircumCircle(app.points[i].x, app.points[i].y,
+                        app.triangles[m].p1.x, app.triangles[m].p1.y, 
+                        app.triangles[m].p2.x, app.triangles[m].p2.y, 
+                        app.triangles[m].p3.x, app.triangles[m].p3.y,
+                        &posxCentre, &posyCentre, &rayon);
+
+            Coords centre = {posxCentre, posyCentre};
+            app.centers.push_back(centre);
+        }
+
+        // ******************************* //
+        // *DIAGRAMME VORONOI AVEC LIGNES* //
+        // ******************************* //
+        
+        if (VORONOI == 1) {
+            // Vider la liste existante de edges
+            app.edges.clear();
+
+            // Pour chaque triangle T déjà créé :
+            for(size_t n = 0; n < app.triangles.size(); n++) {
+                // Vérifiez quels sont les triangles adjacents à celui-ci
+                for(size_t o = 0; o < app.triangles.size(); o++) {
+                    if (n == o) break;
+
+                    int sharedPoints = 0;
+
+                    if (app.triangles[n].p1 == app.triangles[o].p1 || 
+                        app.triangles[n].p1 == app.triangles[o].p2 || 
+                        app.triangles[n].p1 == app.triangles[o].p3)
+                        sharedPoints++;
+
+                    if (app.triangles[n].p2 == app.triangles[o].p1 || 
+                        app.triangles[n].p2 == app.triangles[o].p2 || 
+                        app.triangles[n].p2 == app.triangles[o].p3) 
+                        sharedPoints++;
+
+                    if (app.triangles[n].p3 == app.triangles[o].p1 || 
+                        app.triangles[n].p3 == app.triangles[o].p2 || 
+                        app.triangles[n].p3 == app.triangles[o].p3) 
+                        sharedPoints++;
+
+                    // Si les triangles sont adjacents alors on relie leurs centres
+                    if (sharedPoints == 2) {
+                        app.edges.push_back({{app.centers[n].x, app.centers[n].y}, {app.centers[o].x, app.centers[o].y}});
+                    }
+                }  
+            } 
+        }
+
+        // ********************************** //
+        // *DIAGRAMME VORONOI AVEC POLYGONES* //
+        // ********************************** //
+
+        else if (VORONOI == 2) {
+            // Liste des centres des cercles circonscrits
+            vector<Coords> LC;
+
+            // Parcourir tous les triangles
+            for (size_t p = 0; p < app.triangles.size(); p++) {
+                // Vérifier si le point app.points[i] est présent dans le triangle
+                if (app.triangles[p].p1 == app.points[i] || app.triangles[p].p2 == app.points[i] || app.triangles[p].p3 == app.points[i]) {
+                    // Vérifier si le triangle est adjacent à d'autres triangles
+                    bool isAdjacent = false;
+                    
+                    // Parcourir à nouveau tous les triangles pour les comparer
+                    for (size_t q = 0; q < app.triangles.size(); q++) {
+                        // Ignorer le triangle courant
+                        if (&app.triangles[p] == &app.triangles[q]) {
+                            continue;
+                        }
+                        
+                        // Vérifier si le point app.points[i] est présent dans l'autre triangle
+                        if (app.triangles[q].p1 == app.points[i] || app.triangles[q].p2 == app.points[i] || app.triangles[q].p3 == app.points[i]) {
+                            // Vérifier si les triangles ont des arêtes communes
+                            if ((app.triangles[p].p1 == app.triangles[q].p2 || app.triangles[p].p1 == app.triangles[q].p3 ||
+                                app.triangles[p].p2 == app.triangles[q].p1 || app.triangles[p].p2 == app.triangles[q].p3 ||
+                                app.triangles[p].p3 == app.triangles[q].p1 || app.triangles[p].p3 == app.triangles[q].p2)) {
+                                isAdjacent = true;
+                                break;
+                            }
+                        }
+                    }
+                    
+                    // Si le triangle est adjacent, l'ajouter à la liste
+                    if (isAdjacent) {
+                        LC.push_back(app.centers[p]);
+                    }
+                }
+            }
+
+            // Couleur aléatoire
+            int r = rand() % 256;
+            int g = rand() % 256;
+            int b = rand() % 256;
+
+            // Parcourir les centres
+            for (size_t j = 0; j < LC.size(); j++) {
+                Coords& center1 = LC[j];
+
+                // Parcourir les autres centres
+                for (size_t k = 0; k < LC.size(); k++) {
+                    if (j != k) {
+                        Coords& center2 = LC[k];
+
+                        // Créer un triangle avec les centres et le point
+                        Polygon polygon = {app.points[i], center1, center2, r, g, b};
+                        
+                        // Ajouter le triangle à la liste des polygons
+                        app.polygons.push_back(polygon);
+                    }
+                }
+            } 
         }
     }
 }
 
-bool handleEvent(Application &app)
-{
+bool handleEvent(Application &app) {
     /* Remplissez cette fonction pour gérer les inputs utilisateurs */
     SDL_Event e;
-    while (SDL_PollEvent(&e))
-    {
+    while (SDL_PollEvent(&e)) {
         if (e.type == SDL_QUIT)
             return false;
-        else if (e.type == SDL_WINDOWEVENT_RESIZED)
-        {
+        else if (e.type == SDL_WINDOWEVENT_RESIZED) {
             app.width = e.window.data1;
             app.height = e.window.data1;
         }
-        else if (e.type == SDL_MOUSEWHEEL)
-        {
+        else if (e.type == SDL_MOUSEWHEEL) {
         }
-        else if (e.type == SDL_MOUSEBUTTONUP)
-        {
-            if (e.button.button == SDL_BUTTON_RIGHT)
-            {
+        else if (e.type == SDL_MOUSEBUTTONUP) {
+            if (e.button.button == SDL_BUTTON_RIGHT) {
                 app.focus.x = e.button.x;
                 app.focus.y = e.button.y;
                 app.points.clear();
             }
-            else if (e.button.button == SDL_BUTTON_LEFT)
-            {
+            else if (e.button.button == SDL_BUTTON_LEFT) {
                 app.focus.y = 0;
                 app.points.push_back(Coords{e.button.x, e.button.y});
                 construitVoronoi(app);
@@ -240,8 +412,7 @@ bool handleEvent(Application &app)
     return true;
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
     SDL_Window *gWindow;
     SDL_Renderer *renderer;
     Application app{720, 720, Coords{0, 0}};
@@ -250,8 +421,7 @@ int main(int argc, char **argv)
     // Creation de la fenetre
     gWindow = init("Awesome Voronoi", 720, 720);
 
-    if (!gWindow)
-    {
+    if (!gWindow) {
         SDL_Log("Failed to initialize!\n");
         exit(1);
     }
@@ -259,8 +429,7 @@ int main(int argc, char **argv)
     renderer = SDL_CreateRenderer(gWindow, -1, 0); // SDL_RENDERER_PRESENTVSYNC
 
     /*  GAME LOOP  */
-    while (true)
-    {
+    while (true) {
         // INPUTS
         is_running = handleEvent(app);
         if (!is_running)
